cc821c8152b5c562b66168e083e0885181409f27
Rico <d-Rickyy-b@users.noreply.github.com>
2022-09-20 03:05:44 +0200

    fix: implement new way to obtain token
    Most of the code has been heavily inspired by https://github.com/ianjwhite99/connected-car-node-sdk

--- fordHeaders.js
+++ fordHeaders.js
@@ -1,18 +1,14 @@
 // setup header objects
 const defaultHeaders = new Map([
     ['Accept', '*/*'],
-    ['Accept-Language', 'en-us'],
-    ['Content-Type', 'application/json'],
     ['User-Agent', 'FordPass/5 CFNetwork/1327.0.4 Darwin/21.2.0'],
+    ['Accept-Language', 'en-US,en;q=0.9'],
     ['Accept-Encoding', 'gzip, deflate, br']
 ])
+
+module.exports.defaultHeaders = defaultHeaders
 
 module.exports.fordHeaders = fordHeaders = new Map([
     ...defaultHeaders,
     ['Application-Id', "71A3AD0A-CF46-4CCF-B473-FC7FE5BC4592"]
-])
-
-module.exports.iamHeaders = iamHeaders = new Map([
-    ...defaultHeaders,
-    ['Content-Type', 'application/x-www-form-urlencoded']
 ])
